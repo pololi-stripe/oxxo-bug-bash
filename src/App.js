7445c21973b2ae4d9b3ec418bae8b02dfaf0755c
Polo Li <pololi@stripe.com>
2020-09-14 16:10:06 -0700

    Pl - make get voucher work

--- src/App.js
+++ src/App.js
@@ -1,34 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-class App extends React.Component {
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements, ElementsConsumer } from "@stripe/react-stripe-js";
+
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY, {
+  betas: ["oxxo_pm_beta_1"],
+  apiVersion: "2020-03-02; oxxo_beta=v2",
+});
+
+export class OxxoDemo extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { data: null }
+    this.state = {
+      clientSecret: "",
+      oxxoDetails: "",
+      error: "",
+      hostedVoucherUrl: "",
+    };
   }
 
-  async componentDidMount() {
-    const response = await fetch('/express_backend');
-    const body = await response.json();
+  createPaymentIntent = async () => {
+    const option = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+    };
 
-    console.log(body);
-    console.log(body.message)
+    const res = await fetch("/payment-intent", option);
+    const data = res.json();
+    return data;
+  };
 
-    this.setState({ data: body.message })
-  }
+  getPendingToPay = async () => {
+    const { stripe } = this.props;
+
+    const data = await this.createPaymentIntent();
+    const result = await stripe.confirmOxxoPayment(data.client_secret, {
+      payment_method: {
+        billing_details: {
+          name: "Bug Bash",
+          email: "Bug@bash.com",
+        },
+      },
+    });
+
+    if (result.error) {
+      this.setState({
+        error: result.error.message,
+        clientSecret: data.client_secret,
+      });
+    } else {
+      this.setState({
+        clientSecret: data.client_secret,
+        oxxoDetails: result.paymentIntent.next_action.display_oxxo_details,
+      });
+    }
+  };
 
   render() {
+    // if (!this.props.stripe) {
+    //   return <Alert message="stripe JS instance is not set up." type="error" />;
+    // }
+
     return (
       <div className="App">
-        <header className="App-header">
-          {this.state.data}
-        </header>
+        <header className="App-header">OXXO Bug Bash</header>
+        <button onClick={this.getPendingToPay}>Get Pending To Pay OXXO</button>
       </div>
     );
   }
 }
 
+const InjectedOxxoDemo = () => {
+  return (
+    <ElementsConsumer>
+      {({ elements, stripe }) => (
+        <OxxoDemo elements={elements} stripe={stripe} />
+      )}
+    </ElementsConsumer>
+  );
+};
+
+export function App() {
+  return (
+    <Elements stripe={stripePromise}>
+      <InjectedOxxoDemo />
+    </Elements>
+  );
+}
 
 export default App;
