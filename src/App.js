31fb62463227cd508cae6d16b5debc5ad2c0d332
Polo Li <pololi@stripe.com>
2020-09-14 19:39:19 -0700

    Pl - make three voucher cases nice

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React from "react";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements, ElementsConsumer } from "@stripe/react-stripe-js";
-
 import { createPaymentIntent } from "./helper";
+
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Alert from "react-bootstrap/Alert";
+import Card from "react-bootstrap/Card";
+
+const PENDING_TO_PAY = {
+  name: "Bug Bash",
+  email: "bug_bash@example.com",
+};
+
+const PAID_VOUCHER = {
+  name: "BugBash Paid",
+  email: "bug_bash_succeed_immediately@example.com",
+};
+
+const EXPIRED_VOUCHER = {
+  name: "BugBash Expired",
+  email: "bug_bash_expire_immediately@example.com",
+};
 
 const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY, {
   betas: ["oxxo_pm_beta_1"],
@@ -23,7 +45,7 @@ export class OxxoDemo extends React.Component {
     };
   }
 
-  getPendingToPay = () => {
+  getOxxo = (input) => {
     const paymentIntentData = {
       amount: 2000,
       currency: "mxn",
@@ -44,12 +66,13 @@ export class OxxoDemo extends React.Component {
         .confirmOxxoPayment(object.client_secret, {
           payment_method: {
             billing_details: {
-              name: "Bug Bash",
-              email: "Bug@bash.com",
+              name: input.name,
+              email: input.email,
             },
           },
         })
         .then((result) => {
+          console.log(result);
           if (result.error) {
             this.setState({
               clientSecret: client_secret,
@@ -66,15 +89,86 @@ export class OxxoDemo extends React.Component {
     });
   };
 
-  render() {
-    // if (!this.props.stripe) {
-    //   return <Alert message="stripe JS instance is not set up." type="error" />;
-    // }
+  renderVoucherLinkAlert = () => {
+    if (!this.state.oxxoDetails) {
+      return null;
+    }
+
+    const { hosted_voucher_url } = this.state.oxxoDetails;
 
     return (
-      <div className="App">
-        <header className="App-header">OXXO Bug Bash</header>
-        <button onClick={this.getPendingToPay}>Get Pending To Pay OXXO</button>
+      <Alert variant="info">
+        Open the voucher in a new tab:
+        <Alert.Link href={hosted_voucher_url} target="_blank">
+           hosted voucher link
+        </Alert.Link>
+      </Alert>
+    );
+  };
+
+  render() {
+    if (!this.props.stripe) {
+      return <Alert variant="danger">stripe JS instance is not set up.</Alert>;
+    }
+
+    return (
+      <div>
+        <Container fluid>
+          <Row>
+            <Col>
+              <h1 className="header">OXXO Bug Bash</h1>
+            </Col>
+          </Row>
+        </Container>
+
+        <Container>
+          {this.renderVoucherLinkAlert()}
+          <Card className="card-space">
+            <Card.Header as="h5">Pending To Pay OXXO</Card.Header>
+            <Card.Body>
+              <Card.Text>
+                <p>Name: {PENDING_TO_PAY.name}</p>
+                <p className="ellipsis">Email: {PENDING_TO_PAY.email}</p>
+              </Card.Text>
+              <Button
+                variant="primary"
+                onClick={() => this.getOxxo(PENDING_TO_PAY)}
+              >
+                Go somewhere
+              </Button>
+            </Card.Body>
+          </Card>
+          <Card className="card-space">
+            <Card.Header as="h5">Paid OXXO</Card.Header>
+            <Card.Body>
+              <Card.Text>
+                <p>Name: {PAID_VOUCHER.name}</p>
+                <p className="ellipsis">Email: {PAID_VOUCHER.email}</p>
+              </Card.Text>
+              <Button
+                onClick={() => this.getOxxo(PAID_VOUCHER)}
+                variant="success"
+              >
+                Get Paid OXXO
+              </Button>
+            </Card.Body>
+          </Card>
+          <Card className="card-space">
+            <Card.Header as="h5">Expired OXXO</Card.Header>
+            <Card.Body>
+              <Card.Text>
+                <p>Name: {EXPIRED_VOUCHER.name}</p>
+                <p className="ellipsis">Email: {EXPIRED_VOUCHER.email}</p>
+              </Card.Text>
+              <Button
+                onClick={() => this.getOxxo(EXPIRED_VOUCHER)}
+                variant="warning"
+              >
+                Get Expired OXXO
+              </Button>
+            </Card.Body>
+          </Card>
+        </Container>
       </div>
     );
   }
